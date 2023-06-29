69512f096f0dc43039ed9facd51acfa2063231b6
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2023-06-29 21:08:45 +0700

    feat: cek auth header (belum dicek username dan pass nya apakah sesuai

--- index.ts
+++ index.ts
@@ -1,20 +1,48 @@
-import express from "express"
+import express from "express";
+import bodyParser from "body-parser";
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+//conigure express
+app.use(bodyParser.json());
+//set port
+const port = process.env.PORT || 3000;
 
 //endpoint
 //buat tes submit
-app.post("/test",(req,res)=>{
-  res.send("Tes submit sukses")
-})
+app.post("/test", (req, res) => {
+  //cek authorization header
+  const auth_header = req.headers.authorization;
+  console.log(auth_header);
+  if (!auth_header) {
+    res.status(401).send("Unauthorized to access endpoint");
+    return;
+  }
+  //parse authorization
+  try {
+    //parse username dan password
+    const [user, password] = Buffer.from(auth_header.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    console.log(user, password);
+    //cek username apakah valid
+  } catch (err) {
+    console.log(err);
+    res.status(401).send("Unauthorized to access endpoint");
+    return;
+  }
+  res.status(201);
+  res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+  res.send("Tes submit sukses");
+});
 //buat submit bagian a
-app.post("/submit/a",(req,res)=>{
-  res.send("Congratulations on completing part a!")
-})
+app.post("/submit/a", (req, res) => {
+  res.send("Congratulations on completing part a!");
+});
 //buat submit bagian b
-app.post("/submit/b",(req,res)=>{
-  res.send("Congratulations on completing part b!")
-})
+app.post("/submit/b", (req, res) => {
+  res.send("Congratulations on completing part b!");
+});
 
-app.listen(port,()=>{console.log(`App listening at port ${port}`)})
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
