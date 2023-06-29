3cc6ecfa276e6381891bee91b4b3b080f7a77904
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2023-06-29 20:44:44 +0700

    feat: setup endpoints

--- index.ts
+++ index.ts
@@ -3,8 +3,18 @@ import express from "express"
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/',(req,res)=>{
-  res.send("Masuk")
+//endpoint
+//buat tes submit
+app.post("/test",(req,res)=>{
+  res.send("Tes submit sukses")
+})
+//buat submit bagian a
+app.post("/submit/a",(req,res)=>{
+  res.send("Congratulations on completing part a!")
+})
+//buat submit bagian b
+app.post("/submit/b",(req,res)=>{
+  res.send("Congratulations on completing part b!")
 })
 
 app.listen(port,()=>{console.log(`App listening at port ${port}`)})
